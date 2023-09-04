aae13a1943c1ac81eb1d87a8ca28c610800ae97c
Cong1201 <congsalamander@gmail.com>
2023-09-04 15:28:22 +0700

    up

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+//const cookieParser = require("cookie-parser");
+
+dotenv.config();
+//Create app
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.urlencoded({ limit: "10000mb" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(cookieParser());
+
+routes(app);
+
+mongoose
+  .connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log("Connect Db success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(port, () => {
+  console.log("Server is running in port: ", +port);
+});
